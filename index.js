f869c322a31d71b9ccdc4d9b4da34eed3bca79ad
Mohamad Tassieh <mohamad.bassam.tessieh@gmail.com>
2020-11-25 06:21:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const pets= ["Milo", "Otis", "Garfield"];
 const append=[...pets,"Odie"];
 const prepend=["Odie",...pets];
 
-//const removeLast= pets;
-//removeLast.pop();
+const removeLast= pets;
+removeLast.pop();
 
-//const removeFirst=pets;
-//removeFirst.shift();
+const removeFirst=pets;
+removeFirst.shift();
 
-const removeLast= pets.pop();
+
